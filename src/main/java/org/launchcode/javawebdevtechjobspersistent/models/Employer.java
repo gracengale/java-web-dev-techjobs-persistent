d9527fad3cf05ab321ee0195612a803d5ced2cf1
Grace <gminnis1@yahoo.com>
2020-10-31 11:35:36 -0500

    Set up Employer and Skill Repository/Class and finished AbstractEntity (Part 2 before Controllers)

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,37 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location is required.")
+    private String location;
 
+//    @OneToMany(mappedBy = "employer")
+//    private List<Job> jobs = new ArrayList<>();
+
+    public Employer(String location) {
+        super();
+        this.location = location;
+    }
+
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+//    public List<Job> getJobs() {
+//        return jobs;
+//    }
 }
